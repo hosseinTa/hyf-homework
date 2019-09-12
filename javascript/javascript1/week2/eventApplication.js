d8dfc4d0d155c3f09c57fcbb317df4f842159b20
Hossein <mhtarokh@gmail.com>
2019-09-12 10:52:34 +0200

    Fixing typo in eventApplication.js and add a consile.log(). testing correct detection of today and it seems it works.

--- javascript/javascript1/week2/eventApplication.js
+++ javascript/javascript1/week2/eventApplication.js
@@ -1,6 +1,8 @@
 function getEventWeekday(days) {
-    const week = ['Synday' , 'Monday' , 'Tuesday' , 'Wednesday' , 
-                'Thirsday' , 'Friday' , 'Saturday'];
+    const week = ['Sunday' , 'Monday' , 'Tuesday' , 'Wednesday' , 
+                'Thursday' , 'Friday' , 'Saturday'];
     const today = (new Date()).getDay();
     return week[(today + days ) % 7] ;
 }
+
+console.log(getEventWeekday(6));
