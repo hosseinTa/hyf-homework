9361e6c4735b9e39086b1b0ff83e15d49688eb5b
Hossein <mhtarokh@gmail.com>
2019-09-12 16:20:57 +0200

    eventApplication was updated to include the possibility that user decide if he/she want to personally set today instead of receiving today from system time

--- javascript/javascript1/week2/eventApplication.js
+++ javascript/javascript1/week2/eventApplication.js
@@ -1,8 +1,9 @@
-function getEventWeekday(days) {
+function getEventWeekday(days , today) {
     const week = ['Sunday' , 'Monday' , 'Tuesday' , 'Wednesday' , 
-                'Thursday' , 'Friday' , 'Saturday'];
-    const today = (new Date()).getDay();
+    'Thursday' , 'Friday' , 'Saturday'];
+    today = today || (new Date()).getDay();
     return week[(today + days ) % 7] ;
 }
 
-console.log(getEventWeekday(6));
+console.log(getEventWeekday( 6 ,  ));
+console.log(getEventWeekday( 6 ,  5));
