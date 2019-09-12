72c80a4ea4cd36045bd347b722b0283fa0d5b5c2
Hossein <mhtarokh@gmail.com>
2019-09-12 16:07:44 +0200

    FlightBookingFullName.js is shorter and with no ELSE IF. Console.log was added to test the code

--- javascript/javascript1/week2/flightBookingFullnameFunction.js
+++ javascript/javascript1/week2/flightBookingFullnameFunction.js
@@ -1,15 +1,34 @@
-function getFullname( firstName , sureName , useFormalName) {
-    if( firstName && sureName ) {
+function getFullname( firstName , lastName , useFormalName) {
+    if( firstName && lastName ) {
         if( useFormalName ) {
-            return`Lord ${firstName} ${sureName}`;
+            return `Lord ${firstName} ${lastName}`;
         } else {
-            return`${firstName} ${sureName}`;
+            return `${firstName} ${lastName}`;
         }
-    } else if( firstName ) {
-        return(`Sure name box is empty.`);
-    } else if( sureName ) {
-        return(`First name box is empty.`);
     } else {
-        return(`First name and sure name box is empty.`);
+        if( (firstName + lastName).length === 0 ) {
+            return `First name and last name box is empty.`;
+        } else  {
+            return `Either first name or last name is empty.`;
+        }
     }
 }
+
+
+
+console.log(getFullname( ''     , ''        ));
+console.log(getFullname( 'as'   , ''        ));
+console.log(getFullname( ''     , 'ascx'    ));
+console.log(getFullname( 'ca'   , 'ascx'    ));
+
+
+console.log(getFullname( ''     , ''        , false));
+console.log(getFullname( 'as'   , ''        , false));
+console.log(getFullname( ''     , 'ascx'    , false));
+console.log(getFullname( 'ca'   , 'ascx'    , false));
+
+
+console.log(getFullname( ''     , ''        , true));
+console.log(getFullname( 'as'   , ''        , true));
+console.log(getFullname( ''     , 'ascx'    , true));
+console.log(getFullname( 'ca'   , 'ascx'    , true));
