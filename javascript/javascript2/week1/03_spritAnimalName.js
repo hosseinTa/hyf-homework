89733fee217153919fcfbaae71574f993631d94f
Hossein <mhtarokh@gmail.com>
2019-10-01 14:18:25 +0200

    HW2 of Week1 of JS2 is completed. only the optional part is left.

--- javascript/javascript2/week1/03_spritAnimalName.js
+++ javascript/javascript2/week1/03_spritAnimalName.js
@@ -1,6 +1,14 @@
 const mybutton = document.querySelector('button');
-console.log(mybutton);
+const myOutput = document.getElementById('result');
 
-mybutton.addEventListener( 'click' , () => sayHello() ) ;
+mybutton.addEventListener( 'click' , sayHello ) ;
 
-function sayHello(){   console.log("Hello World");  }
+function sayHello(){
+    const spritalNames = ['Happy Bear' , 'running Wolf' , 'fighting rabit' , 'smart spider' , 'rainy night bat' , 'horny horse' , 'hairy gurilla' , 'loyal dog' , 'fat pig' , 'killer shark'];
+    const userName = document.getElementById('userName').value;
+    if (userName === '') {
+        myOutput.textContent =  'Enter your name in the box';    
+    } else {
+        myOutput.textContent =  userName + ' - The ' + spritalNames[Math.floor(10*Math.random())];
+    }
+}
