171ec6bc9ab79f84834d182272c55fcebcadfef3
Hossein <mhtarokh@gmail.com>
2019-10-03 22:20:45 +0200

    update the radio selector of the spritAnimalName fixed CLICK and MOUSEOVER

--- javascript/javascript2/week1/03_spritAnimalName.js
+++ javascript/javascript2/week1/03_spritAnimalName.js
@@ -1,30 +1,31 @@
 const mybutton = document.querySelector('button');
 const myOutput = document.getElementById('result');
 const radioClick = document.getElementsByName('triger');
-console.log(radioClick);
-
-mybutton.addEventListener( 'click' , sayHello ) ;
+const userName = document.getElementById('userName');
 
 function sayHello(){
     const spritalNames = ['Happy Bear' , 'running Wolf' , 'fighting rabit' , 'smart spider' , 'rainy night bat' , 'horny horse' , 'hairy gurilla' , 'loyal dog' , 'fat pig' , 'killer shark'];
-    const userName = document.getElementById('userName').value;
-    if (userName === '') {
+    if (userName.value === '') {
         myOutput.textContent =  'Enter your name in the box';    
     } else {
-        myOutput.textContent =  userName + ' - The ' + spritalNames[Math.floor(10*Math.random())];
+        myOutput.textContent =  userName.value + ' - The ' + spritalNames[Math.floor(10*Math.random())];
     }
 }
 
 function switchToClick() {
-    console.log('click');
+    mybutton.addEventListener( 'click' , sayHello );
+    mybutton.style.visibility = 'visible' ;
+    userName.removeEventListener( 'mouseenter' , sayHello );
 }
 
 
 function switchToHover() {
-    console.log('hover');
+    userName.addEventListener( 'mouseenter' , sayHello );
+    mybutton.removeEventListener( 'click' , sayHello );
+    mybutton.style.visibility = 'hidden' ;
 }
 
-
 function switchToType() {
-    console.log('type');
+    userName.removeEventListener( 'mouseenter' , sayHello );
+    mybutton.removeEventListener( 'click' , sayHello );
 }
