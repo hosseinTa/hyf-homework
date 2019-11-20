8056fa1a345482a0d56f9b7682ccf91af8add548
hossein.Ta <mhtarokh@gmail.com>
2019-11-20 22:18:11 +0100

    Trying to have more consistent spacing

--- javascript/javascript2/week2/doubleOddNumbers/script.js
+++ javascript/javascript2/week2/doubleOddNumbers/script.js
@@ -1,5 +1,5 @@
-let numbers = [1, 2, 3, 4, 5, 6, 7, 9, 12, 11, 18, 3, 27, 10];
+let numbers = [ 1, 2, 3, 4, 5, 6, 7, 9, 12, 11, 18, 3, 27, 10 ];
 
 const newNumbers =  numbers.filter( number => number%2 === 1)
-        .map( number => number *2 )
-console.log(newNumbers);
+        .map ( number => number * 2 )
+console.log ( newNumbers );
