e5002423b1a87bb5baec1867b5f1038b1bdf1c20
hossein.Ta <mhtarokh@gmail.com>
2019-11-23 19:27:14 +0100

    Copleted first section of HW of Week2 of DataBase

--- databases/week2/DB_W2_HW.sql
+++ databases/week2/DB_W2_HW.sql
@@ -16,3 +16,21 @@ SET description = 'Change the title of a task'
 WHERE (id = 37);
 
 -- 3. Change the task due date with these attributes: taskID, newDueDate
+UPDATE task
+SET due_date = due_date + INTERVAL 20 DAY
+WHERE (id = 37);
+-- I intentionally were trying to see if I can add for example 20 days to the old due_date.
+
+-- 4. Change the task status with these attributes: taskID, newStatus
+UPDATE task
+SET status_id = status_id + 1
+WHERE (id = 37);
+
+-- 5. Mark a task as complete with this attribute: taskID
+UPDATE task
+SET status_id = 3
+WHERE (id = 37);
+
+-- 6. Delete task with this attribute: taskID
+DELETE FROM task
+WHERE id = 37;
